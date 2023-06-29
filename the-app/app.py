0953f180e07b3cf6233d0fdc121a895c7fdbc04c
root <jeka010@walla.com>
2023-06-29 14:16:04 +0300

    python app with mysql

--- the-app/app.py
+++ the-app/app.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
-# Define the username and password
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{os.environ['MYSQL_USER']}:{os.environ['MYSQL_PASSWORD']}@{os.environ['MYSQL_HOST']}:{os.environ['MYSQL_PORT']}/{os.environ['MYSQL_DB']}"
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+
 USERNAME = 'admin'
 PASSWORD = 'password'
 
@@ -22,4 +32,5 @@ def dashboard():
     return 'Welcome to the dashboard'
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(host='0.0.0.0', port=5000)
