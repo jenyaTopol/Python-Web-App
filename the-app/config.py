038d756fdd3b035a0f7b51d4f815f6c9a14db1fb
jenyatopol <jeka010@walla.com>
2023-09-04 11:19:07 +0300

    working app no and db no dockercompose

--- the-app/config.py
+++ the-app/config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql://dbuser:dbpassword@mysql:3306/mydatabase'
+    SQLALCHEMY_DATABASE_URI = 'mysql://dbuser:dbpassword@mysql:3306/mydatabase'#your myssql username, password, adress, port ,db-name 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
